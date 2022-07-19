a19fbbd2200a70574aac5f95f38c17dee1abe9fc
박형주 <hyeongju1122@gmail.com>
2022-07-19 21:31:54 +0900

    annotation plus

--- src/DataType/IteratorType.java
+++ src/DataType/IteratorType.java
@@ -15,17 +15,25 @@ import java.util.Set;
 * 2. hasNext()를 써서 더 많은 요소가 있는지 확인가능!
 */
 
+/* Set 자료형도 알아보자!
+* HashSet은 Set 컬렉션을 구현하는 대표적인 클래스이다.
+* 데이터를 중복 저장할 수 없고 순서를 보장하지 않는다.
+* Set은 인덱스로 객체를 관리하지 않기 때문에 데이터를 검색하기 위해서는 iterator() 메서드로
+* Iterator(반복자)를 생성하고 데이터를 가져와야 한다.
+* */
+
 public class IteratorType {
     public static void main(String[] args) {
 //      Collection 생성
 //      ArrayList<String> aircraft = new ArrayList<>();
+//      Set => 중복생성은 막아줄게 대신 순서는 내맘대로다?
         Set<String> aircraft = new HashSet<>();
         aircraft.add("A380");
         aircraft.add("B747");
         aircraft.add("A350");
         aircraft.add("B777");
 
-//      Iterator 획득!
+//      Iterator 획득! / 반복자 생성!
         Iterator<String> iterator = aircraft.iterator();
 
 //      while문을 사용한경우
--- src/DataType/MapEntry.java
+++ src/DataType/MapEntry.java
@@ -23,6 +23,7 @@ public class MapEntry {
     *   }
     * */
 
+//      Entery는 두개의 데이터가 쌍으로 존재할 때 두 자료형을 묶는 자료형입니다.
         System.out.println("변경전");
         System.out.println("entrySet()은 key와 Value 모두 출력");
         System.out.println("keySet()은 key값만 출력!");
@@ -41,12 +42,23 @@ public class MapEntry {
             System.out.printf("[GATE 번호 : %s\t도착공항 : %s]\n", m.getKey(), m.getValue());
         }
 
+/* Entry => <K,V>묶음 / String과 String을 묶어줬음!
+* set 가방에는 Entry 자료형의 요소만이 들어올 수 있고
+* hasMap.entrySet();를 통해 hasMap안의 key-value요소를 가져와 set에 넣어줍니다.
+*/
         Set<Entry<String, String>> set = hashMap.entrySet();
 
-        Iterator<Map.Entry<String, String>> iterator = set.iterator();
+//      iterator또한 제네릭을 entry로 설정해주고 set.iterator()로 등록합니다.
+        Iterator<Entry<String, String>> iterator = set.iterator();
 
+//      hasNext() => 가져올 iterator이 더이상 없을때까지! / jsp 프로그래밍의 rs.next()와 비슷!
         while(iterator.hasNext()){
-            Map.Entry<String, String> temp = iterator.next();
+            /*
+            *  iterator.next()로 하나씩 가져오는 데이터는 모두 Entry<String, String> (묶음)자료형입니다.
+            *  때문에 temp의 자료형도 Entry<String, String>으로 선언해줍니다.
+            *  그리고 가져온 temp를 토대로 temp.getKey(), temp.getValue()를 통해 key와 value데이터에 각각 접근합니다.
+            * */
+            Entry<String, String> temp = iterator.next();
             System.out.printf("GateNo. =>  %s\t 도착공항 => %s\t", temp.getKey(), temp.getValue());
         }
     }
